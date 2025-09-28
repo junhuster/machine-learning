66c11f112082306e972c51a22187539dbdcf7707
book <1214376534@qq.com>
2025-09-28 20:40:01 +0800

    fix rnn

--- tf-dev/d2l/pytools/d2l.py
+++ tf-dev/d2l/pytools/d2l.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import random
 import time
+import requests
+import os
+import re
+import hashlib
+import collections
+import math
 from IPython import display
+
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+DATA_HUB['time_machine'] = (DATA_URL + 'timemachine.txt',
+                                '090b5e7e70c295757f55df93cb0a180b9691891a')
 
 def set_figsize(figsize=(5.5, 3.5)):  
     plt.rcParams['figure.figsize'] = figsize
@@ -345,3 +356,252 @@ class TrainCallback(tf.keras.callbacks.Callback):  #@save
                   f'test acc {metrics[2]:.3f}')
             print(f'{num_examples / self.timer.avg():.1f} examples/sec on '
                   f'{str(self.device_name)}')
+
+def download(name, cache_dir=os.path.join('../..', 'data')):  #@save
+    """下载一个DATA_HUB中的文件，返回本地文件名"""
+    assert name in DATA_HUB, f"{name} 不存在于 {DATA_HUB}"
+    url, sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir, exist_ok=True)
+    fname = os.path.join(cache_dir, url.split('/')[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname  # 命中缓存
+    print(f'正在从{url}下载{fname}...')
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
+
+
+############language model tool begin ######################
+
+def tokenize(lines, token='word'):  #@save
+    """将文本行拆分为单词或字符词元"""
+    if token == 'word':
+        return [line.split() for line in lines]
+    elif token == 'char':
+        return [list(line) for line in lines]
+    else:
+        print('错误：未知词元类型：' + token)
+
+class Vocab:  #@save
+    """文本词表"""
+    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        # 按出现频率排序
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(), key=lambda x: x[1],
+                                   reverse=True)
+        # 未知词元的索引为0
+        self.idx_to_token = ['<unk>'] + reserved_tokens
+        self.token_to_idx = {token: idx
+                             for idx, token in enumerate(self.idx_to_token)}
+        for token, freq in self._token_freqs:
+            if freq < min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    @property
+    def unk(self):  # 未知词元的索引为0
+        return 0
+
+    @property
+    def token_freqs(self):
+        return self._token_freqs
+
+def count_corpus(tokens):  #@save
+    """统计词元的频率"""
+    # 这里的tokens是1D列表或2D列表
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        # 将词元列表展平成一个列表
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+def load_corpus_time_machine(max_tokens=-1):  #@save
+    """返回时光机器数据集的词元索引列表和词表"""
+    lines = read_time_machine()
+    tokens = tokenize(lines, 'char')
+    vocab = Vocab(tokens)
+    # 因为时光机器数据集中的每个文本行不一定是一个句子或一个段落，
+    # 所以将所有文本行展平到一个列表中
+    corpus = [vocab[token] for line in tokens for token in line]
+    if max_tokens > 0:
+        corpus = corpus[:max_tokens]
+    return corpus, vocab
+
+def read_time_machine():  #@save
+    """将时间机器数据集加载到文本行的列表中"""
+    with open(download('time_machine'), 'r') as f:
+        lines = f.readlines()
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+
+def seq_data_iter_random(corpus, batch_size, num_steps):  #@save
+    """使用随机抽样生成一个小批量子序列"""
+    # 从随机偏移量开始对序列进行分区，随机范围包括num_steps-1
+    corpus = corpus[random.randint(0, num_steps - 1):]
+    # 减去1，是因为我们需要考虑标签
+    num_subseqs = (len(corpus) - 1) // num_steps
+    # 长度为num_steps的子序列的起始索引
+    initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+    # 在随机抽样的迭代过程中，
+    # 来自两个相邻的、随机的、小批量中的子序列不一定在原始序列上相邻
+    random.shuffle(initial_indices)
+
+    def data(pos):
+        # 返回从pos位置开始的长度为num_steps的序列
+        return corpus[pos: pos + num_steps]
+
+    num_batches = num_subseqs // batch_size
+    for i in range(0, batch_size * num_batches, batch_size):
+        # 在这里，initial_indices包含子序列的随机起始索引
+        initial_indices_per_batch = initial_indices[i: i + batch_size]
+        X = [data(j) for j in initial_indices_per_batch]
+        Y = [data(j + 1) for j in initial_indices_per_batch]
+        yield tf.constant(X), tf.constant(Y)
+
+def seq_data_iter_sequential(corpus, batch_size, num_steps):  #@save
+    """使用顺序分区生成一个小批量子序列"""
+    # 从随机偏移量开始划分序列
+    offset = random.randint(0, num_steps)
+    num_tokens = ((len(corpus) - offset - 1) // batch_size) * batch_size
+    Xs = tf.constant(corpus[offset: offset + num_tokens])
+    Ys = tf.constant(corpus[offset + 1: offset + 1 + num_tokens])
+    Xs = tf.reshape(Xs, (batch_size, -1))
+    Ys = tf.reshape(Ys, (batch_size, -1))
+    num_batches = Xs.shape[1] // num_steps
+    for i in range(0, num_batches * num_steps, num_steps):
+        X = Xs[:, i: i + num_steps]
+        Y = Ys[:, i: i + num_steps]
+        yield X, Y
+
+class SeqDataLoader:  #@save
+    """加载序列数据的迭代器"""
+    def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
+        if use_random_iter:
+            self.data_iter_fn = seq_data_iter_random
+        else:
+            self.data_iter_fn = seq_data_iter_sequential
+        self.corpus, self.vocab = load_corpus_time_machine(max_tokens)
+        self.batch_size, self.num_steps = batch_size, num_steps
+
+    def __iter__(self):
+        return self.data_iter_fn(self.corpus, self.batch_size, self.num_steps)
+
+def load_data_time_machine(batch_size, num_steps,  #@save
+                           use_random_iter=False, max_tokens=10000):
+    """返回时光机器数据集的迭代器和词表"""
+    data_iter = SeqDataLoader(
+        batch_size, num_steps, use_random_iter, max_tokens)
+    return data_iter, data_iter.vocab
+
+############language model tool end######################
+
+
+###################rnn begin########################
+def predict_ch8(prefix, num_preds, net, vocab):
+    state = net.begin_state(batch_size=1)
+    outputs = [vocab[prefix[0]]]
+    get_input = lambda : tf.reshape(tf.constant([outputs[-1]]), (1,1)).numpy()
+    for y in prefix[1:]:
+        _,state = net(get_input(), state)
+        outputs.append(vocab[y])
+    for _ in range(num_preds):
+        y,state = net(get_input(), state)
+        outputs.append(int(y.numpy().argmax(axis=1).reshape(1)))
+    return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+##fix  原书中的代码计算出的norm始终小于1.0，导致没有实际走到梯度裁剪的地方。
+##fix  此处对原有的代码进行了调整
+def grad_clipping(grads, theta):
+    theta = tf.constant(theta, dtype=tf.float32)
+    new_grads = []
+    for grad in grads:
+        if isinstance(grad, tf.IndexedSlices):
+            new_grads.append(tf.convert_to_tensor(grad))
+        else:
+            new_grads.append(grad)
+
+    norm = tf.math.sqrt(sum((tf.reduce_sum(grad**2)).numpy() for grad in new_grads))
+    norm = tf.cast(norm, dtype=tf.float32)
+
+    theta_f = 1.0
+    if norm < 1.0:
+        theta_f = norm + 0.0001
+    else:
+        theta_f = theta / norm
+
+    for i,grad in enumerate(new_grads):
+        new_grads[i] = grad * theta_f
+    return new_grads
+
+
+def train_epoch_ch8(net, train_iter, loss, optimizer, user_random_iter, vocab_size):
+    state, timer = None, Timer()
+    metric = Accumulator(2)  
+    for X,Y in train_iter:
+        if state is None or user_random_iter:
+            state = net.begin_state(X.shape[0])
+        with tf.GradientTape(persistent=True) as g:
+            y_hat, state = net(X, state)
+            #keras v3下不需要先对Y执行tf.transponse()操作了
+            #一开始没去掉之前训练一直不收敛，去掉后，收敛了
+            y = tf.reshape(Y, (-1))
+            l = loss(y, y_hat)
+        params = net.trainable_variables
+        grads = g.gradient(l, params)
+        grads = grad_clipping(grads, 1)
+        optimizer.apply_gradients(zip(grads, params))
+    metric.add(l * len(y),  len(y))
+    return math.exp(metric[0] / metric[1]), metric[1] / timer.stop()
+
+#@save
+def train_ch8(net, train_iter, vocab, lr, num_epochs, strategy,
+              use_random_iter=False):
+    """训练模型（定义见第8章）"""
+    with strategy.scope():
+        loss = tf.keras.losses.SparseCategoricalCrossentropy(
+            from_logits=True)
+        updater = tf.keras.optimizers.SGD(lr)
+    animator = Animator(xlabel='epoch', ylabel='perplexity',
+                            legend=['train'], xlim=[10, num_epochs])
+    predict = lambda prefix: predict_ch8(prefix, 50, net, vocab)
+    # 训练和预测
+    for epoch in range(num_epochs):
+        ppl, speed = train_epoch_ch8(net, train_iter, loss, updater,
+                                     use_random_iter, len(vocab))
+        if (epoch + 1) % 10 == 0:
+            print(predict('time traveller'))
+            animator.add(epoch + 1, [ppl])
+    device = try_gpu()._device_name
+    print(f'困惑度 {ppl:.1f}, {speed:.1f} 词元/秒 {str(device)}')
+    print(predict('time traveller '))
+    print(predict('traveller '))
+
+
+###################rnn end########################

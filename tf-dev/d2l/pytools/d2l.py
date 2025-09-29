cd97260c29447212826a2dfab2b68c342d8fee48
book <1214376534@qq.com>
2025-09-29 10:38:13 +0800

    up

--- tf-dev/d2l/pytools/d2l.py
+++ tf-dev/d2l/pytools/d2l.py
@@ -620,4 +620,22 @@ class RNNModelScratch:
     def begin_state(self, batch_size):
         return self.init_state(batch_size, self.num_hiddens)
 
+#@save
+class RNNModel(tf.keras.layers.Layer):
+    def __init__(self, rnn_layer, vocab_size, **kwargs):
+        super(RNNModel, self).__init__(**kwargs)
+        self.rnn = rnn_layer
+        self.vocab_size = vocab_size
+        self.dense = tf.keras.layers.Dense(vocab_size)
+
+    def call(self, inputs, state):
+        X = tf.one_hot(inputs, self.vocab_size)
+        # rnn返回两个以上的值
+        Y, *state = self.rnn(X, state)
+        output = self.dense(tf.reshape(Y, (-1, Y.shape[-1])))
+        return output, state
+
+    def begin_state(self, *args, **kwargs):
+        return self.rnn.cell.get_initial_state(*args, **kwargs)
+
 ###################rnn end########################

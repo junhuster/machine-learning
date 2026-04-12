f09d443707294d879f8aadbc4a475b91cb08e344
book <1214376534@qq.com>
2026-04-12 13:46:42 +0800

    gpt2

---
+++ llm-learn/llm-from-scratch/logger.py
@@ -0,0 +1,21 @@
+import logging as log
+from logging.handlers import TimedRotatingFileHandler
+
+logfile="/home/ubuntu/work/logs/gpt2-train.log"
+handler = TimedRotatingFileHandler(
+    filename=logfile,
+    when="midnight",      # 每天午夜轮转
+    interval=1,           # 每1天
+    backupCount=5,        # 保留5个备份
+    encoding="utf-8"
+)
+handler.suffix = "%Y-%m-%d"
+formatter = log.Formatter(
+    fmt="%(asctime)s - %(name)s - %(levelname)s [%(filename)s:%(lineno)d %(funcName)s] - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+handler.setFormatter(formatter)
+log.basicConfig(
+    level=log.DEBUG,
+    handlers=[handler]
+)
---
+++ llm-learn/llm-from-scratch/train-gpt-2.py
@@ -0,0 +1,99 @@
+import sys 
+import os
+import tiktoken
+import torch
+import logging as log
+from pathlib import Path
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+sys.path.append(str(Path(__file__).parent.parent))
+import logger
+import gpt_2 as GPT2
+
+start_text = "Every effort moves you"
+train_file_path = "/home/ubuntu/work/data/llm-data/train_data/The-Count-of-Monte-Cristo.txt"
+load_local_model = True
+local_model_path = "/home/ubuntu/work/data/llm-data/local_model/gpt2/model.pth"
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 256, # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
+}
+
+def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
+    fig, ax1 = plt.subplots(figsize=(5, 3))
+
+    # Plot training and validation loss against epochs
+    ax1.plot(epochs_seen, train_losses, label="Training loss")
+    ax1.plot(epochs_seen, val_losses, linestyle="-.", label="Validation loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+    ax1.legend(loc="upper right")
+    ax1.xaxis.set_major_locator(MaxNLocator(integer=True))  # only show integer labels on x-axis
+
+    # Create a second x-axis for tokens seen
+    ax2 = ax1.twiny()  # Create a second x-axis that shares the same y-axis
+    ax2.plot(tokens_seen, train_losses, alpha=0)  # Invisible plot for aligning ticks
+    ax2.set_xlabel("Tokens seen")
+
+    fig.tight_layout()  # Adjust layout to make room
+    plt.savefig("loss-plot.pdf")
+    plt.show()
+
+if __name__ == "__main__":
+    file_path = train_file_path
+    #url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch02/01_main-chapter-code/the-verdict.txt"
+
+    if not os.path.exists(file_path):
+        with urllib.request.urlopen(url) as response:
+            text_data = response.read().decode('utf-8')
+        with open(file_path, "w", encoding="utf-8") as file:
+            file.write(text_data)
+    else:
+        with open(file_path, "r", encoding="utf-8") as file:
+            text_data = file.read()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    train_ratio = 0.90
+    split_idx = int(train_ratio * len(text_data))
+    train_data = text_data[:split_idx]
+    val_data = text_data[split_idx:]
+    tokenizer = tiktoken.get_encoding("gpt2")
+    #torch.manual_seed(123)
+
+    train_loader = GPT2.create_dataloader_v1(
+        train_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=True,
+        shuffle=True,
+        num_workers=0
+    )
+
+    val_loader = GPT2.create_dataloader_v1(
+        val_data,
+        batch_size=2,
+        max_length=GPT_CONFIG_124M["context_length"],
+        stride=GPT_CONFIG_124M["context_length"],
+        drop_last=False,
+        shuffle=False,
+        num_workers=0
+    )
+    model = GPT2.GPTModel(GPT_CONFIG_124M)
+    model.to(device)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=0.0001, weight_decay=0.1)
+    num_epochs = 20
+    train_losses, val_losses, tokens_seen = GPT2.train_model_simple(
+        model, train_loader, val_loader, optimizer, device,
+        num_epochs=num_epochs, eval_freq=5, eval_iter=5,
+        start_context= start_text, tokenizer=tokenizer
+    )
+
+    epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+    plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+    GPT2.clear_memory()
+
